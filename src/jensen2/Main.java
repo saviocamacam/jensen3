2b85dc831753684163c80b1f2123480701507966
saviocamacam <saviocamacam@alunos.utfpr.edu.br>
2016-09-27 22:19:26 -0300

    Aplicado o uso do Produtor em threads a espera de uma interrupção do usuario para parar o processo de producao de transacoes

--- src/jensen2/Main.java
+++ src/jensen2/Main.java
@@ -1,27 +1,25 @@
 package jensen2;
 
+import java.util.Scanner;
+
 public class Main {
 	private static int numeroItens = 3;
 	private static int numeroTransacoes = 4;
 	private static int numeroAcessos = 9;
-	private static GerenciadorTransacao gerenciador;
-
+	private static Scanner scanner;
+	
 	public static void main(String[] args) {
-		int ultimoIndice = 0;
+		scanner = new Scanner(System.in);
 		System.out.println( "Criando transacoes e gravando no banco..." );
-		try {
-			do {
-				ultimoIndice = TransacaoDao.pegarUltimoIndice();
-				gerenciador = new GerenciadorTransacao(numeroItens, numeroTransacoes, numeroAcessos, ultimoIndice);
-				Schedule schedule = new Schedule(gerenciador.getListaTransacoes());
-				TransacaoDao.gravarTransacoes(schedule);
-				//System.out.println( "ok" );
-				Thread.sleep( 10 * 1000 );
-				//System.out.println( "ok" );
-			} while(true);
-		}catch (InterruptedException e) {
-			e.printStackTrace();
- 		}
+		Produtor produtor = new Produtor(numeroItens, numeroTransacoes, numeroAcessos);
+		produtor.start();
+		System.out.println("Pressione Enter para encerrar a producao!");
+		
+		if(scanner.hasNextLine()) {
+			System.out.println("Producao encerrada");
+			produtor.setFlag(false);
+		}
+		
 	}
 
 }
--- src/jensen2/statemens.sql
+++ src/jensen2/statemens.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS schedule;
 CREATE TABLE schedule (
   idOperacao SERIAL,
   indiceTransacao INTEGER,
